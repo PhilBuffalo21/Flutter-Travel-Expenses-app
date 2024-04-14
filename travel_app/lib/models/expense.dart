6192bcbec6b839fd753900b57402c69fc35bb715
KrisTheMuggle <kris.themuggle@gmail.com>
2024-04-14 16:20:00 +1000

    populate expense.dart with fields

--- travel_app/lib/models/expense.dart
+++ travel_app/lib/models/expense.dart
@@ -1,12 +1,16 @@
+import 'package:uuid/uuid.dart';
+
 enum Category { food, experience, shopping, accommendation }
+
+const uuid = Uuid();
 
 class Expense {
   Expense(
-      {required this.id,
-      required this.title,
+      {required this.title,
       required this.date,
       required this.amount,
-      required this.category});
+      required this.category})
+      : id = uuid.v1();
   final String id;
   final String title;
   final double amount;
