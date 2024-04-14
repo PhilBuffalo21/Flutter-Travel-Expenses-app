08b23b69d70b6c61893dcae15f2a5ecdedb8229d
KrisTheMuggle <kris.themuggle@gmail.com>
2024-04-14 15:41:16 +1000

    add basic main and app controller files

---
+++ travel_app/lib/expense_controller.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Controller extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _Controller();
+  }
+}
+
+class _Controller extends State<Controller> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Expenses");
+  }
+}
